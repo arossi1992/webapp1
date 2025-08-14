3f07d854b2f3238eeb36c4de48cd397a1c21b16d
earojas4 <erojas4@utexas.edu>
2025-08-14 00:51:59 -0500

    Initial

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = r'C:\Users\Tony\PycharmProjects\webapp1\todos.txt'
+
+
+def get_todos(filepath = FILEPATH):
+    """Reads a text file and returns the list of to-do items."""
+    with open(filepath, 'r') as file_local:
+            todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath = FILEPATH):
+    """ Writes the to-do item list to a text file."""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
